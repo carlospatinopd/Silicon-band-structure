cc7f0d7ae55f19de1f5815dada0c2bfc2441c2c5
carlospatinopd <c.patino02@ufromail.cl>
2024-12-23 01:35:06 -0300

    fix figures

--- k_points_energy.m
+++ k_points_energy.m
@@ -5,11 +5,11 @@ k_points = [2,3,4,5,6,7,8,9,10];
 final_energy = [-22.4803692297,-22.6259070131,-22.6585633143,-22.6680541949,-22.6712003683,-22.6723399614,-22.6727808267,-22.6729600573,-22.6730358353];
 
 figure(1)
-plot(k_points,final_energy,'o--',LineWidth=1.5,MarkerSize=6)
+plot(k_points,final_energy,'o--',LineWidth=3,MarkerSize=10)
 ax = gca;
-ax.XAxis.FontSize = 16;
-ax.YAxis.FontSize = 16;
-ylabel("Final Energy (Ry)",FontSize=20)
-xlabel("k-points grid size",FontSize=20)
+ax.XAxis.FontSize = 20;
+ax.YAxis.FontSize = 20;
+ylabel("Final Energy (Ry)",FontSize=32)
+xlabel("k-points grid size",FontSize=32)
 fig = gcf;
 fig.Position(3:4)=[1000,800];
--- rho_cutoff.m
+++ rho_cutoff.m
@@ -5,12 +5,12 @@ cutoff_number = [160, 180, 200, 220, 240];
 final_energy = [-22.6585633143, -22.6585595501, -22.6585580159, -22.6585658449, -22.6585653458];
 
 figure(1)
-plot(cutoff_number,final_energy,'o--',LineWidth=1.5,MarkerSize=6)
+plot(cutoff_number,final_energy,'o--',LineWidth=3,MarkerSize=10)
 ax = gca;
-ax.XAxis.FontSize = 16;
-ax.YAxis.FontSize = 16;
-ylabel("Final Energy (Ry)",FontSize=20)
-xlabel("Charge Density Energy Cutoff (Ry)",FontSize=20)
+ax.XAxis.FontSize = 20;
+ax.YAxis.FontSize = 20;
+ylabel("Final Energy (Ry)",FontSize=32)
+xlabel("Charge Density Energy Cutoff (Ry)",FontSize=32)
 ylim([-22.66,-22.65])
 fig = gcf;
 fig.Position(3:4)=[1000,800];
